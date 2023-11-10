85bfa92ff6084ad4415dedb0f6bb1d9d28167f75
diasakosd <damianosdiasakos@gmail.com>
2023-11-10 12:40:10 +0200

    files

---
+++ script.js
@@ -0,0 +1,17 @@
+function login() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    // Add your login logic here
+    console.log("Login clicked");
+}
+
+function register() {
+    var name = document.getElementById("name").value;
+    var lastname = document.getElementById("lastname").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    // Add your registration logic here
+    console.log("Register clicked");
+}
