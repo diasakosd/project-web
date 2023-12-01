b6ed55d09f7b674aab5813b5b0118d0a773a67eb
AlkisD <alkisd123@gmail.com>
2023-12-01 20:56:46 +0200

    kk

--- rescuer/resc_load_manag.php
+++ rescuer/resc_load_manag.php
@@ -2,16 +2,19 @@
 include 'session_rescuer.php';
 $_SESSION['site'] = '../rescuer/rescuer.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
-    <!-- Add your stylesheets here -->
+    
+    <!-- Adjust the paths to your CSS file and Leaflet map script -->
     <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 </head>
+
 <body>
     <div class="navbar">
         <img class="logo" src="Screenshot_5.png" alt="Logo">
@@ -19,19 +22,22 @@ $_SESSION['site'] = '../rescuer/rescuer.php';
         <a href="resc_see_cargo.php">See cargo</a>
         <a href="resc_load_manag.php">Load cargo</a>
         <a href="rescuer.php">Home</a>
-    </div>
+    </div>   
+    
     <div class="container">
         <div class="header">
+            
         </div>
+        <br><br>
         <div class="content">
-            <!-- Add your citizen-specific content here -->
-            <p>This is your rescuer page. Add more content as needed.</p>
+            <p>This is your rescuer page for cargo management. Add more content as needed.</p>
         </div>
-        <div class="logout">
-            <p><a href="../logout.php">Logout</a></p>
-        </div>
+
     </div>
-    <!-- Add your scripts here -->
+<br>
+<br>
+<br><br>
+
     <script src="rescuer-details-script.js"></script>
 </body>
 </html>
